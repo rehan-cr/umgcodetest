adf07579389f7e802dd1242f50722e3ebb24e919
rehan-cr <rehan@coderapper.com>
2023-07-27 01:09:18 +0530

    Loading spinner added to the upsell add to cart button

--- assets/cart-upsell.js
+++ assets/cart-upsell.js
@@ -2,7 +2,8 @@
 const upsellContainer = document.querySelector('.cart-upsell__container')
 const upsellBtn = upsellContainer.querySelector('.upsell-product__atc--button')
 upsellBtn ? upsellBtn.addEventListener('click', upsellAddToCart) : null
-
+const upsellBtnLabel = document.querySelector('.upsell-atc__btn--label')
+const upsellLoading = document.querySelector('.upsell-product__atc--button .loading-overlay__spinner')
 
 // section rendering with the help of Shopify Section API
 const sectionRender = () => {
@@ -24,12 +25,14 @@ function upsellAddToCart (e) {
   e.preventDefault()
   const upsellProduct = upsellContainer.querySelector('.upsell-product__title')
   const variantId = upsellProduct.dataset.variantId;
-  console.log(variantId)
 
   const data = {
     id: variantId,
     quantity: 1,
   };
+
+  upsellBtnLabel.classList.add('hidden')
+  upsellLoading.classList.remove('hidden')
 
   fetch(`${routes.cart_add_url}.js`, {
     method: "POST",
@@ -42,6 +45,9 @@ function upsellAddToCart (e) {
     .then((upsellResponse) => {
       console.log('Upsell item added:', upsellResponse);
       sectionRender()
+      upsellBtnLabel.classList.remove('hidden')
+      upsellBtnLabel.innerHTML = 'Added to cart'
+      upsellLoading.classList.add('hidden')
     })
     .catch((error) => {
       console.error('Error updating cart item:', error);
